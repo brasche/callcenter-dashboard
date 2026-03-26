985f7b5b88a13c325e6732d93c6b5e9d12b58eb4
brasche <ericd.brasch@gmail.com>
2026-03-26 12:43:35 -0700

    Run all syncs immediately on startup instead of waiting for first interval

--- app/services/scheduler.py
+++ app/services/scheduler.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from app.config import BLUEROCK_SYNC_INTERVAL, SHEETS_SYNC_INTERVAL
 
@@ -9,10 +10,11 @@ def start_scheduler():
     from app.services.bluerock import sync_bluerock, poll_agent_status, sync_agent_daily_stats
     from app.services.sheets import sync_sheets
 
-    scheduler.add_job(sync_bluerock,         "interval", seconds=BLUEROCK_SYNC_INTERVAL, id="bluerock_sync",   max_instances=1)
-    scheduler.add_job(sync_sheets,           "interval", seconds=SHEETS_SYNC_INTERVAL,   id="sheets_sync",     max_instances=1)
-    scheduler.add_job(poll_agent_status,     "interval", seconds=30,                     id="status_poll",     max_instances=1)
-    scheduler.add_job(sync_agent_daily_stats,"interval", seconds=300,                    id="daily_stats_sync",max_instances=1)
+    now = datetime.now()
+    scheduler.add_job(sync_bluerock,         "interval", seconds=BLUEROCK_SYNC_INTERVAL, id="bluerock_sync",   max_instances=1, next_run_time=now)
+    scheduler.add_job(sync_sheets,           "interval", seconds=SHEETS_SYNC_INTERVAL,   id="sheets_sync",     max_instances=1, next_run_time=now)
+    scheduler.add_job(poll_agent_status,     "interval", seconds=30,                     id="status_poll",     max_instances=1, next_run_time=now)
+    scheduler.add_job(sync_agent_daily_stats,"interval", seconds=300,                    id="daily_stats_sync",max_instances=1, next_run_time=now)
     scheduler.start()
 
 
